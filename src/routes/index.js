9ff496fc45e80c7232ed6470d05738e349bdcada
Truongwinner <pham.truong142022@gmail.com>
2022-08-28 20:00:05 +0700

    Step_3  Cấu hình Router/Layout cho dự án

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+import { HeaderOnlyDefault } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// Route này quản lý các trang không cần đăng nhập vẫn truy cập được
+const publicRoutes = [
+    // mảng này là nơi mà Dev sẽ tự do định nghĩa các chức năng bằng cách thêm các key vào Object
+    // tương ứng
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnlyDefault },
+    { path: '/search', component: Search, layout: null },
+];
+
+// Route này quản lý các trang cần phải đăng nhập vẫn truy cập được. Nếu Click vào thì sẽ bị điều hướng đến trang đăng ký
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
