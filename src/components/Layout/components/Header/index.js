6579a0026f0cade77f4f7f4cbf2ab6020550538e
Truongwinner <pham.truong142022@gmail.com>
2022-08-29 22:04:09 +0700

    Step_5 Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 // Tham khảo cách dùng classNames.bind(style) ở đường dẫn bên dưới
 //https://www.tabnine.com/code/javascript/functions/classnames/ClassNamesBind/bind
@@ -8,7 +12,24 @@ const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt={'tiktok'} />
+                </div>
+
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
